06b3413f9a1265f7b8cecf8f4e78c65ceeeb5715
Mustaphatemitope <hiqmahmustapha@gmail.com>
2022-10-24 13:34:50 -0700

    corrections on main.h file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,8 +6,8 @@
 
 #include <stdio.h>
 
-void reset_to_98(int *n)
-void swap_int(int *a, int *b)
-int _strlen(char *s)
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
 
 #endif /* MAIN_H */
